1eaec6e8689d6675e8851b0bc1459131f2b1d144
Mickey Iluz <mixi.il@gmail.com>
2018-07-16 05:50:25 +0300

    add bench2 (vector hsv2rgb)

--- rgb2hsv.cc
+++ rgb2hsv.cc
@@ -23,6 +23,69 @@ typedef struct HsvColor
     HsvColor() : h(0), s(0), v(0) { }
 } HsvColor;
 
+
+void HsvToRgb(const vec_u8& vh, const vec_u8& vs, const vec_u8& vv,
+              vec_u8& vr, vec_u8& vg, vec_u8& vb)
+{
+    // TODO: check vector sizes
+
+    for (size_t i = 0; i < vh.size(); i++) {
+       
+        HsvColor hsv;
+        hsv.h = vh[i];
+        hsv.s = vs[i];
+        hsv.v = vv[i];
+        
+        RgbColor rgb;
+        unsigned char region, remainder, p, q, t;
+
+        if (hsv.s == 0)
+        {
+            rgb.r = hsv.v;
+            rgb.g = hsv.v;
+            rgb.b = hsv.v;
+            vr[i] = rgb.r;
+            vg[i] = rgb.g;
+            vb[i] = rgb.b;
+            continue;
+        }
+
+        region = hsv.h / 43;
+        remainder = (hsv.h - (region * 43)) * 6; 
+
+        p = (hsv.v * (255 - hsv.s)) >> 8;
+        q = (hsv.v * (255 - ((hsv.s * remainder) >> 8))) >> 8;
+        t = (hsv.v * (255 - ((hsv.s * (255 - remainder)) >> 8))) >> 8;
+
+        switch (region)
+        {
+            case 0:
+                rgb.r = hsv.v; rgb.g = t; rgb.b = p;
+                break;
+            case 1:
+                rgb.r = q; rgb.g = hsv.v; rgb.b = p;
+                break;
+            case 2:
+                rgb.r = p; rgb.g = hsv.v; rgb.b = t;
+                break;
+            case 3:
+                rgb.r = p; rgb.g = q; rgb.b = hsv.v;
+                break;
+            case 4:
+                rgb.r = t; rgb.g = p; rgb.b = hsv.v;
+                break;
+            default:
+                rgb.r = hsv.v; rgb.g = p; rgb.b = q;
+                break;
+        }
+
+        vr[i] = rgb.r;
+        vg[i] = rgb.g;
+        vb[i] = rgb.b;
+    }
+}
+
+
 RgbColor HsvToRgb(HsvColor hsv)
 {
     RgbColor rgb;
@@ -139,7 +202,28 @@ void RgbToHsv(const ::vector)
 #endif
 
 
-int main(int argc, char *argv[])
+void bench2()
+{
+    const size_t BSZ = 7200;
+    vec_u8 vr(BSZ, 0);
+    vec_u8 vg(BSZ, 0);
+    vec_u8 vb(BSZ, 0);
+    vec_u8 vh(BSZ, 0);
+    vec_u8 vs(BSZ, 0);
+    vec_u8 vv(BSZ, 0);
+
+    LOGF("start");
+    HsvToRgb(vh, vs, vv, vr, vg, vb);
+    LOGF("end");
+    
+    unsigned int sum = 0;
+    for (size_t i = 0; i < vr.size(); i++) {
+      sum += vr[i];
+    }
+}
+
+
+void bench1()
 {
     std::vector<HsvColor> vhsv(7200);
     std::vector<RgbColor> vrgb(7200);  
@@ -158,5 +242,10 @@ int main(int argc, char *argv[])
     for (size_t i =0; i < vrgb.size(); i++) {
         sum += vrgb[i].r;
     }
+}
+
+int main()
+{
+    bench2();
     return 0;
 }
